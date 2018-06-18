b79b3aa7cdfae018e3571af35c1c1d216dc44a1f
Anthony Scopatz <scopatz@gmail.com>
2018-06-18 12:21:11 -0400

    added setup.py

---
+++ setup.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+import os
+from setuptools import setup
+
+
+VERSION = '0.0.0'
+
+install_file = os.path.join(os.path.dirname(__file__), 'requirements', 'run.txt')
+with open(install_file) as f:
+    install_reqs = f.read()
+install_reqs = install_reqs.split()
+
+setup_kwargs = {
+    "name": 'libcflib',
+    "packages": ['libcflib'],
+    "long_description": open('README.md').read(),
+    "version": VERSION,
+    "description": 'Library Conda Forge Library',
+    "license": 'BSD 3-clause',
+    "author": 'The Regro developers',
+    "author_email": 'conda-forge@googlegroups.com',
+    "url": 'https://github.com/regro/libcflib',
+    "download_url": "https://github.com/ergs/conda-forge/zipball/" + VERSION,
+    "classifiers": [
+        "License :: OSI Approved",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python",
+        "Topic :: Utilities",
+        ],
+    "zip_safe": False,
+    "data_files": [("", ['LICENSE', 'README.md']),],
+    "scripts": [],
+    "install_requires": install_reqs,
+    }
+
+if __name__ == '__main__':
+    setup(
+        **setup_kwargs
+        )
