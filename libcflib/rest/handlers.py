11438337d5e87cdc0d90ce0ce747121adc031608
Anthony Scopatz <scopatz@gmail.com>
2018-06-18 19:46:33 -0400

    more handlers

--- libcflib/rest/handlers.py
+++ libcflib/rest/handlers.py
@@ -2,4 +2,19 @@
 from libcflib.rest.request_handler import RequestHandler
 
 
+NON_EMPTY_STR = {'type': 'string', 'empty': False, 'required': True}
+
+
+class Artifact(RequestHandler):
+    """Gets an artifact's data"""
+
+    route = '/artifact'
+    schema = {'package': NON_EMPTY_STR.copy(),
+              'channel': NON_EMPTY_STR.copy(),
+              'arch': NON_EMPTY_STR.copy(),
+              'name': NON_EMPTY_STR.copy(),
+              }
+
+    def get(self, *args, **kwargs):
+        """GETs an artifact."""
 
