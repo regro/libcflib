a1d4943395263c4619bd63d62654e8d698f3cef1
christopher <cjwright4242@gmail.com>
2018-06-19 16:02:43 -0400

    fix schema

--- libcflib/bootstrap_schema.py
+++ libcflib/bootstrap_schema.py
@@ -1,5 +1,6 @@
-from collections.abc import MutableMapping, Sequence
+from collections.abc import MutableMapping
 
+from libcflib.tools import is_non_string_non_dict_iter
 
 PY_TO_CERB = {int: "integer", str: "string", float: "float", set: "set", bool: "bool"}
 
@@ -9,7 +10,7 @@ def recursive_schema(d):
     for k, v in d.items():
         if isinstance(v, MutableMapping):
             schema[k] = {"type": "dict", "schema": recursive_schema(v)}
-        elif isinstance(v, Sequence):
+        elif is_non_string_non_dict_iter(v):
             schema[k] = {
                 "type": "list",
                 "schema": {"type": PY_TO_CERB[type(next(iter(v)))]},
