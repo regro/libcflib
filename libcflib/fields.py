81e61eb6f35ea22f70f198b926431bd254d67d90
Justin Calamari <justin.calamari@gmail.com>
2018-06-19 13:31:40 -0400

    FieldType for dictionary

---
+++ libcflib/fields.py
@@ -0,0 +1,22 @@
+from whoosh.fields import (FieldType, TEXT, KEYWORD, BOOLEAN, NUMERIC)
+
+
+TYPE_MAP = {'string': TEXT,
+            'list': KEYWORD,
+            'set': KEYWORD,
+            'bool': BOOLEAN,
+            'integer': NUMERIC,
+            'dict': DICT}
+
+
+class DICT(FieldType):
+    def __init__(self, schema, stored=False):
+        self.schema = schema
+
+    def subfields():
+        for k, v in self.schema.items():
+            try:
+                subfield = TYPE_MAP[v['type']](schema=v['schema'], stored=self.stored)
+            except (TypeError, NameError):
+                subfield = TYPE_MAP[v['type']](stored=self.stored)
+            yield k, subfield
