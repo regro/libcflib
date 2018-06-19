0bd8d97013e5e5350fa259d041620088acb558b8
christopher <cjwright4242@gmail.com>
2018-06-19 18:50:50 -0400

    Package now takes data from top level json

--- libcflib/model.py
+++ libcflib/model.py
@@ -6,17 +6,74 @@ from typing import Iterator
 from collections import defaultdict
 from collections.abc import MutableMapping
 
+import networkx as nx
 
-class Package(object):
-    def __init__(self, *, name=None, artifact_ids=None):
+
+class Package(MutableMapping):
+    def __init__(self, *, name=None, artifact_ids=None, channel='conda-forge'):
         self.name = name
+        self._path = channel+'.json'
+        self._load()
         self.artifacts = defaultdict(lambda: defaultdict(set))
         for a in artifact_ids:
             _, channel, arch, artifact_name = a.split("/", 3)
             self.artifacts[channel][arch].add(artifact_name)
 
+    def __iter__(self) -> Iterator:
+        if not self._loaded:
+            self._load()
+        yield from self._d
+
+    def __len__(self) -> int:
+        if not self._loaded:
+            self._load()
+        return len(self._d)
+
+    def __getitem__(self, key):
+        if not self._loaded:
+            self._load()
+        return self._d[key]
+
+    def __setitem__(self, key, value) -> None:
+        if not self._loaded:
+            self._load()
+        self._d[key] = value
+
+    def __delitem__(self, key) -> None:
+        if not self._loaded:
+            self._load()
+        del self._d[key]
+
+    def __getattr__(self, name):
+        if name.startswith("_") or name in self.__dict__:
+            return self.__dict__[name]
+        else:
+            if not self._loaded:
+                self._load()
+            return self._d[name]
+
+    def __setattr__(self, name, value):
+        if name.startswith("_") or name in self.__dict__:
+            self.__dict__[name] = value
+        else:
+            self._d[name] = value
+
+    def __hash__(self):
+        return hash(self._path)
+
     def __repr__(self):
         return f"Package({self.name}"
+
+    def _load(self):
+        env = builtins.__xonsh_env__
+        filename = os.path.join(env.get("LIBCFGRAPH_DIR"), self._path)
+        with open(filename, "r") as f:
+            self._d.update(nx.node_link_graph(json.load(f))[self.name])
+        self._loaded = True
+
+    def asdict(self,):
+        """Returns the dictionary view of the package"""
+        return self._d
 
 
 class Artifact(MutableMapping):
