c98f11349980195212f463624d7fce541e0e2c53
Anthony Scopatz <scopatz@gmail.com>
2018-06-19 13:41:25 -0400

    black

--- tests/test_model.py
+++ tests/test_model.py
@@ -11,8 +11,8 @@ def test_node(tmppkgdir):
         json.dump(d, f)
 
     env = builtins.__xonsh_env__
-    dirs = str(tmppkgdir).split('/')
-    env['LIBCFGRAPH_DIR'] = '/'.join(dirs[:-3])
+    dirs = str(tmppkgdir).split("/")
+    env["LIBCFGRAPH_DIR"] = "/".join(dirs[:-3])
     pkg, channel, arch = dirs[-3:]
     n = Artifact(pkg=pkg, channel=channel, arch=arch, name="a")
     assert n.a == "hi"
