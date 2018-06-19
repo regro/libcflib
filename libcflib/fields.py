08c7629b0b457583f8d2e04b8a83838c48cf141a
Justin Calamari <justin.calamari@gmail.com>
2018-06-19 15:04:22 -0400

    Properly add DICT to TYPE_MAP

--- libcflib/fields.py
+++ libcflib/fields.py
@@ -5,18 +5,23 @@ TYPE_MAP = {'string': TEXT,
             'list': KEYWORD,
             'set': KEYWORD,
             'bool': BOOLEAN,
-            'integer': NUMERIC,
-            'dict': DICT}
+            'integer': NUMERIC}
 
 
 class DICT(FieldType):
+    type_map = TYPE_MAP
+
     def __init__(self, schema, stored=False):
         self.schema = schema
+        self.type_map['dict'] = DICT
 
     def subfields(self):
         for k, v in self.schema.items():
             try:
-                subfield = TYPE_MAP[v['type']](schema=v['schema'], stored=self.stored)
+                subfield = self.type_map[v['type']](schema=v['schema'], stored=self.stored)
             except (TypeError, NameError):
-                subfield = TYPE_MAP[v['type']](stored=self.stored)
+                subfield = self.type_map[v['type']](stored=self.stored)
             yield k, subfield
+
+
+TYPE_MAP['dict'] = DICT
