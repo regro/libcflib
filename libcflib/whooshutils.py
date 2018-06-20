68821afe801a7e89812edf21dff30e2cdb982f9e
Justin Calamari <justin.calamari@gmail.com>
2018-06-20 01:11:34 -0400

    Use NestedIndex

--- libcflib/whooshutils.py
+++ libcflib/whooshutils.py
@@ -1,38 +1,43 @@
 import os.path
 
-from whoosh.index import open_dir, exists_in, create_in
+from whoosh.index import exists_in
 from libcflib.fields import TYPE_MAP, NestedSchema
+from libcflib.index import NestedIndex
 
 
 def create_whoosh_schema(schema):
-    fields = {k: TYPE_MAP[v['type']] for k, v in schema.items()}
+    fields = {k: TYPE_MAP[v["type"]] for k, v in schema.items()}
     for f, t in fields.items():
         try:
-            fields[f] = t(schema=schema[f]['schema'])
+            fields[f] = t(schema=schema[f]["schema"], stored=True)
         except (TypeError, KeyError):
-            pass
+            fields[f] = t(stored=True)
+        fields[f].stored = True
     return NestedSchema(**fields)
 
 
-def get_index(index, schema):
+def get_index(index, schema=None):
+    from whoosh.filedb.filestore import FileStorage
+
+    indexname = "MAIN"
+    storage = FileStorage(index)
     if not os.path.exists(index):
         os.mkdir(index)
     if exists_in(index):
-        return open_dir(index)
+        return NestedIndex(storage, schema=schema, indexname=indexname)
     else:
-        return create_in(index, schema)
+        return NestedIndex.create(storage, schema, indexname)
 
 
-def add(index, **kwargs):
-    ix = get_index(index)
+def add(index, schema=None, **kwargs):
+    ix = get_index(index, schema)
     writer = ix.writer()
-    doc = {k: v for k, v in kwargs.items() if k in ix.schema.names()}
-    writer.add_document(**doc)
+    writer.add_document(**kwargs)
     writer.commit()
 
 
-def add_from(index, docs):
-    ix = get_index(index)
+def add_from(index, docs, schema=None):
+    ix = get_index(index, schema)
     writer = ix.writer()
     for doc in docs:
         doc = {k: v for k, v in doc.items() if k in ix.schema.names()}
