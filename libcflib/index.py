562c27a8492d4bc11c8889fb72b476603464584e
Justin Calamari <justin.calamari@gmail.com>
2018-06-20 13:40:26 -0400

    Document index

--- libcflib/index.py
+++ libcflib/index.py
@@ -1,7 +1,10 @@
+"""Custom whoosh indexes."""
 from whoosh.index import FileIndex
 
 
 class NestedIndex(FileIndex):
+    """A whoosh index that uses a NestedWriter to add documents with a nested structure."""
+
     def writer(self, procs=1, **kwargs):
         from libcflib.writing import NestedWriter
 
