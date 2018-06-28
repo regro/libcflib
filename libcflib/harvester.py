eb4fe51d8702417201fc48638384065d85d50941
Justin Calamari <justin.calamari@gmail.com>
2018-06-28 03:48:20 -0400

    Map feedstock to packages

--- libcflib/harvester.py
+++ libcflib/harvester.py
@@ -23,22 +23,25 @@ def filter_file(filename):
     else:
         return True
 
+
 # There are some meta.yaml files that were written weirdly and have some strange tag
-# information in them.  This is not parsable by safe loader, so we add some more 
+# information in them.  This is not parsable by safe loader, so we add some more
 # fallback loading
 def yaml_construct_fallback(loader, node):
     return None
 
+
 ruamel_yaml.add_constructor(
-    'tag:yaml.org,2002:python/object/apply:builtins.getattr', 
+    "tag:yaml.org,2002:python/object/apply:builtins.getattr",
     yaml_construct_fallback,
     constructor=ruamel_yaml.SafeConstructor,
 )
 ruamel_yaml.add_constructor(
-    'tag:yaml.org,2002:python/object:__builtin__.instancemethod',
+    "tag:yaml.org,2002:python/object:__builtin__.instancemethod",
     yaml_construct_fallback,
     constructor=ruamel_yaml.SafeConstructor,
 )
+
 
 def harvest(io_like):
     tf = tarfile.open(fileobj=io_like, mode="r:bz2")
