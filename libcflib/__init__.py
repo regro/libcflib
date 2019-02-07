614cbb3954044132a020a33d309af35f5e5958b4
Anthony Scopatz <scopatz@gmail.com>
2019-02-07 15:54:42 -0500

    bumped version to 0.0.7

--- libcflib/__init__.py
+++ libcflib/__init__.py
@@ -12,4 +12,4 @@ setup()
 del setup
 
 
-__version__ = '0.0.6'
+__version__ = '0.0.7'
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import os
 from setuptools import setup
 
 
-VERSION = '0.0.6'
+VERSION = '0.0.7'
 
 install_file = os.path.join(os.path.dirname(__file__), "requirements", "run.txt")
 with open(install_file) as f:
