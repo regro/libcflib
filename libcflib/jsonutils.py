c3dd8e14ce1aaf94c6c8336c33c64da271218b32
Christopher J. Wright <cjwright4242@gmail.com>
2020-11-22 16:07:03 -0500

    remove file listing

--- libcflib/jsonutils.py
+++ libcflib/jsonutils.py
@@ -35,14 +35,14 @@ def object_hook(dct):
 def dumps(obj, sort_keys=True, separators=(",", ":"), default=default, **kwargs):
     """Returns a JSON string from a Python object."""
     return json.dumps(
-        obj, sort_keys=sort_keys, separators=separators, default=default, **kwargs
+        obj, sort_keys=sort_keys, separators=separators, default=default, indent=1, **kwargs
     )
 
 
 def dump(obj, fp, sort_keys=True, separators=(",", ":"), default=default, **kwargs):
     """Returns a JSON string from a Python object."""
     return json.dump(
-        obj, fp, sort_keys=sort_keys, separators=separators, default=default, **kwargs
+        obj, fp, sort_keys=sort_keys, separators=separators, default=default, indent=1, **kwargs
     )
 
 
